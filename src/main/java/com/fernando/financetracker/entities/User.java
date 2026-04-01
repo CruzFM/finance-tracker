246980b09f619fe4c3e3b8370e6efe8bb5376bee
Fernando Cruz <cruzfernandomartin94@gmail.com>
2026-03-31 22:27:11 -0300

    F-395 - [Fix] Postgres user table creation

--- src/main/java/com/fernando/financetracker/entities/User.java
+++ src/main/java/com/fernando/financetracker/entities/User.java
@@ -1,22 +1,24 @@
 package com.fernando.financetracker.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.*;
 
 import java.util.Date;
 import java.util.UUID;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue
     private UUID id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(unique = true, nullable = false)
     private String email;
 
-    @Column(name = "password_hash")
+    @Column(name = "password_hash", nullable = false)
     private String passwordHash;
 
     @Column(name = "created_at")
@@ -38,28 +40,24 @@ public class User {
     public void setName(String name){
         this.name = name;
     }
-
     public String getEmail(){
         return email;
     }
     public void setEmail(String email){
         this.email = email;
     }
-
     public String getPasswordHash(){
         return passwordHash;
     }
     public void setPasswordHash(String passwordHash){
         this.passwordHash = passwordHash;
     }
-
     public Date getCreatedAt(){
         return createdAt;
     }
     public void setCreated_at(Date createdAt){
         this.createdAt = createdAt;
     }
-
     public Date getUpdatedAt(){
         return updatedAt;
     }
